8c75b37f57a2829b356e9fc3b76e41ac83165fdd
riseagain47 <riseagain47@gmail.com>
2019-03-09 12:26:47 +0100

    Setup Express Server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+// server.js
+import express from 'express';
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  return res.status(200).send({'message': 'YAY! Congratulations! Your Server is working'});
+})
+
+const port = process.env.PORT || 7440;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
