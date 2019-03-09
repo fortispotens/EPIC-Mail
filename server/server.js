7ebd42a6faf174b64e6ef9bf3337f6cd60676b53
riseagain47 <riseagain47@gmail.com>
2019-03-09 13:00:20 +0100

    Setup Eslint

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 // server.js
 import express from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.status(200).send({'message': 'YAY! Congratulations! Your Server is working'});
-})
+app.get('/', (req, res) => res.status(200).send({ message: 'YAY! Congratulations! Your Server is working' }));
 
 const port = process.env.PORT || 7440;
 app.listen(port, () => {
