c3276ccc953a77f9531af871bad667181330da0a
riseagain47 <riseagain47@gmail.com>
2019-03-10 17:39:39 +0100

    Add dummy contact message sent user files

---
+++ server/utils/contactsDummyData.js
@@ -0,0 +1,16 @@
+export default {
+  contacts: [
+    {
+      id: 1,
+      email: 'abel@email.com',
+      firstName: 'Abel',
+      lastName: 'John'
+    },
+    {
+      id: 2,
+      email: 'cain@email.com',
+      firstName: 'Cain',
+      lastName: 'Adam'
+    }
+  ]
+};
---
+++ server/utils/userDummyData.js
@@ -0,0 +1,18 @@
+export default {
+  user: [
+    {
+      id: 1,
+      email: 'abc@code.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: '123'
+    },
+    {
+      id: 2,
+      email: 'efg@codedev.com',
+      firstName: 'Mark',
+      lastName: 'Thomas',
+      password: '456'
+    }
+  ]
+};
