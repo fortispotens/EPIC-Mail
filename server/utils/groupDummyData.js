fadb94a2dc5785a8084094356bc274d02b71bc67
riseagain47 <riseagain47@gmail.com>
2019-03-10 17:54:30 +0100

    Add inbox group groupmembers dummy data

---
+++ server/utils/groupDummyData.js
@@ -0,0 +1,12 @@
+export default {
+  group: [
+    {
+      id: 1,
+      name: 'First'
+    },
+    {
+      id: 2,
+      name: 'Second'
+    }
+  ]
+};
---
+++ server/utils/inboxDummyData.js
@@ -0,0 +1,14 @@
+export default {
+  inbox: [
+    {
+      receiverId: 1,
+      messageId: '1',
+      createdOn: new Date().toLocaleDateString(),
+    },
+    {
+      receiverId: 2,
+      messageId: '2',
+      createdOn: new Date().toLocaleDateString(),
+    }
+  ]
+};
