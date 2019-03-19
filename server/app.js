d9374970c54cb992831fa78de35b682676eb94d1
riseagain47 <riseagain47@gmail.com>
2019-03-19 12:13:07 +0100

    [Delivers #164725374]

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import messageRoutes from './v1/routes/messages.route';
 import UserRoutes from './v1/routes/users.route';
 import messageRoutesv2 from './v2/routes/messages.route';
 import UserRoutesv2 from './v2/routes/users.route';
+import GroupRoutesv2 from './v2/routes/groups.route';
 
 const app = express();
 
@@ -19,5 +20,6 @@ app.use('/api/v1/messages/', messageRoutes);
 app.use('/api/v1/auth/', UserRoutes);
 app.use('/api/v2/messages/', messageRoutesv2);
 app.use('/api/v2/auth/', UserRoutesv2);
+app.use('/api/v2/groups/', GroupRoutesv2);
 
 export default app;
