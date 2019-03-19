bf1dd3ce882e60e12d5bc06f1485211bcd739310
riseagain47 <riseagain47@gmail.com>
2019-03-19 14:20:15 +0100

    Feat(user) - fetch all groups [Delivers #164728144]

--- server/v2/routes/groups.route.js
+++ server/v2/routes/groups.route.js
@@ -6,6 +6,7 @@ import GroupController from '../controllers/group.controller';
 const router = Router();
 
 router.post('/', GroupController.createNewGroup);
+router.get('/', GroupController.fetchAllGroups);
 
 
 export default router;
