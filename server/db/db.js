bc5700d0137640f88584200371d1f78545821158
riseagain47 <riseagain47@gmail.com>
2019-03-21 10:56:49 +0100

    Chore(server) setup database integration [Delivers #164725004]

---
+++ server/db/db.js
@@ -0,0 +1,38 @@
+import client from '../connectDB';
+
+const createTables = () => {
+  const tables = `
+  DROP TABLE IF EXISTS users CASCADE;
+  DROP TABLE IF EXISTS messages CASCADE;
+
+    CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(128) UNIQUE,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        password VARCHAR NOT NULL
+      );
+
+      CREATE TABLE messages(
+        id SERIAL PRIMARY KEY,
+        createdOn TIMESTAMP WITH TIME ZONE,
+        subject VARCHAR(100) NOT NULL,
+        message VARCHAR(500) NOT NULL,
+        status VARCHAR(10) NOT NULL DEFAULT 'UNREAD'
+      );
+  `;
+
+  return client.query(tables)
+    .then((res) => {
+      console.log('All tables were created successfully!');
+      return process.exit();
+    })
+    .catch((err) => {
+      console.log('Error occured while creating the tables: ', err);
+      client.end();
+      return process.exit();
+    });
+};
+
+
+createTables();
