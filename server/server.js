9c3a21647f56d1c9fc7e880d9699ca35853026dd
riseagain47 <riseagain47@gmail.com>
2019-03-22 00:46:15 +0100

    Modify server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
+server.listen(process.env.PORT || port, () => {
   console.log(`Server is listening on port ${port}`);
 });
