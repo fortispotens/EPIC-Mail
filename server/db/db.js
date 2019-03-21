7af76494ab5d7db3f1c6b3f4019d34e3d688dc2c
riseagain47 <riseagain47@gmail.com>
2019-03-21 11:56:18 +0100

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -4,30 +4,29 @@ const createTables = () => {
   const tables = `
   DROP TABLE IF EXISTS users CASCADE;
   DROP TABLE IF EXISTS messages CASCADE;
+  DROP TABLE IF EXISTS groups CASCADE;
 
-    CREATE TABLE users(
-        id SERIAL PRIMARY KEY,
-        email VARCHAR(128) UNIQUE,
-        firstname VARCHAR(30) NOT NULL,
-        lastname VARCHAR(30) NOT NULL,
-        password VARCHAR NOT NULL
-      );
+  CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(128) UNIQUE,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    password VARCHAR NOT NULL
+  );
 
-      CREATE TABLE messages(
-        id SERIAL PRIMARY KEY,
-        role TIMESTAMP WITH TIME ZONE,
-        subject VARCHAR(100) NOT NULL,
-        message VARCHAR(500) NOT NULL,
-        status VARCHAR(10) NOT NULL DEFAULT 'UNREAD'
-      );
+  CREATE TABLE messages(
+    id SERIAL PRIMARY KEY,
+    createdOn TIMESTAMP WITH TIME ZONE,
+    subject VARCHAR(100) NOT NULL,
+    message VARCHAR(500) NOT NULL,
+    status VARCHAR(10) NOT NULL DEFAULT 'UNREAD'
+  );
 
-      CREATE TABLE groups(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        groupOwnerId VARCHAR(100) NOT NULL,
-      );
-
-
+  CREATE TABLE groups(
+    id SERIAL PRIMARY KEY,
+    name  VARCHAR(100) NOT NULL,
+    groupOwnerId VARCHAR(100) NOT NULL
+  );
   `;
 
   return client.query(tables)
